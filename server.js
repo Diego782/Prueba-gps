6f84dff2de4f4ac8474bac30a5bbe646f132cd05
Diego <diegolara7820@gmail.com>
2024-11-01 02:03:41 -0500

    Actualizado el servidor HTTP para escuchar en el puerto 3000

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const net = require('net');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const PORT = 80;
+const PORT = 3000;
 const TCP_PORT = 4000;
 const HOST = '0.0.0.0';
 
